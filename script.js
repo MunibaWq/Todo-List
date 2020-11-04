946f68ad5dc0ff8d140a1d21b4fd3386c15e2409
MunibaWq <muniba.wq@gmail.com>
2020-11-03 17:49:18 -0700

    css update

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function displayTodo(todo) {
     newListElement.innerHTML = `
         <input id= "${todo.id}" type="checkbox"/>
         <label for="${todo.id}" class = "tick "></label>
-         <div class="text" >${todo.text}</div>
+         <label class="text" >${todo.text}</label>
         <button class="delete-todo js-delete-todo">X</button>
       
       `;
